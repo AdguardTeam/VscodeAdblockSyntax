7dd06a48fe0b97ce9ad23f052419e32c2df8720d
scripthunter7 <57285466+scripthunter7@users.noreply.github.com>
2023-04-27 12:25:43 +0200

    add grammar tools

---
+++ tools/grammar-converter.ts
@@ -0,0 +1,17 @@
+import { parse } from 'yaml';
+import { build } from 'plist';
+
+/**
+ * Converts a YAML grammar file into a PList representation
+ *
+ * @param yamlContent Raw YAML content from the grammar file
+ * @returns PList representation of the grammar
+ * @throws If the YAML content is syntactically invalid
+ */
+export function convertYamlToPlist(yamlContent: string): string {
+    // Parse the YAML content into a JavaScript object
+    const grammar = parse(yamlContent);
+
+    // Convert the JavaScript object into a PList representation
+    return build(grammar);
+}
