aa3985e77830f56566dcb18586e6dd636ab3d241
scripthunter7 <57285466+scripthunter7@users.noreply.github.com>
2023-04-28 11:20:20 +0200

    disable verbose mode
    in fact it is sufficient to show only the names of the tests, no more details are needed for the moment

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    verbose: true,
+    verbose: false,
     testTimeout: 30000,
     testMatch: ['**/test/**/*.test.ts'],
 };
