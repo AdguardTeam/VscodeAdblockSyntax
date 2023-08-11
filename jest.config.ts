c68ec3360b7c96d6d2cda0085b6cba6b9f97ae59
scripthunter7 <d.tota@adguard.com>
2023-08-11 14:11:35 +0200

    Integrate swc into jest (#108)

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from '@jest/types';
+import type { Config } from 'jest';
 
-const config: Config.InitialOptions = {
-    preset: 'ts-jest',
+const config: Config = {
     testEnvironment: 'node',
-    verbose: false,
     testTimeout: 30000,
     testMatch: ['**/test/**/*.test.ts'],
+    // Speed up tests by using SWC instead of Babel
+    transform: { '.ts': '@swc/jest' },
 };
 
 export default config;
