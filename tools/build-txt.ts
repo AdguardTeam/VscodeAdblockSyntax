0353fe08071abbfcb927c01d11d409cf8035e9fc
Slava Leleka <v.leleka@adguard.com>
2023-11-07 20:24:02 +0300

    add build specs
    Squashed commit of the following:
    
    commit a28afe261dac9b48c7c90276fd38bc3db761f2b2
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Tue Nov 7 19:09:55 2023 +0200
    
        fix dist to out dirname
    
    commit 4f4deb5c3623d320ab8167e1705012a01b8cb367
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Tue Nov 7 19:01:09 2023 +0200
    
        add build specs

---
+++ tools/build-txt.ts
@@ -0,0 +1,40 @@
+/**
+ * @file Output the version number to a build.txt file
+ */
+import fs from 'fs';
+import path from 'path';
+
+const UPPER_LEVEL = '../';
+
+const OUTPUT_FOLDER_NAME = 'out';
+const OUTPUT_FILE_NAME = 'build.txt';
+const PKG_FILE_NAME = 'package.json';
+
+// Computed constants
+const outputFolderLocation = path.join(__dirname, UPPER_LEVEL, OUTPUT_FOLDER_NAME);
+const pkgFileLocation = path.join(__dirname, UPPER_LEVEL, PKG_FILE_NAME);
+
+// Read package.json
+const pkg = JSON.parse(fs.readFileSync(pkgFileLocation, 'utf-8'));
+
+if (!pkg.version) {
+    throw new Error('Missing required field "version" in package.json');
+}
+
+const main = (): void => {
+    const content = `version=${pkg.version}`;
+
+    // Create the output folder if it doesn't exist
+    if (!fs.existsSync(outputFolderLocation)) {
+        fs.mkdirSync(outputFolderLocation);
+    }
+
+    // Write the output file
+    const file = path.resolve(outputFolderLocation, OUTPUT_FILE_NAME);
+    fs.writeFileSync(file, content);
+
+    // eslint-disable-next-line no-console
+    console.log(`Wrote ${content} to ${file} was successful`);
+};
+
+main();
